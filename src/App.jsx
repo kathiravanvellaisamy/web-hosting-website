3b74513b78d60e28cd12e325334305abd94f0f33
Kathiravan <kathiravanvellaisamy@gmail.com>
2025-11-04 10:04:41 +0530

    Initial Setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+const App = () => {
+  return (
+    <div className="my-12 max-w-4xl mx-auto py-12">
+      <h1 className="text-5xl text-red-500 text-center">Reliable Web Hosting & Cloud Services</h1>
+      <h2 className="text-slate-700 text-center text-9xl">Host Nexa</h2>
+    </div>
+  )
+}
+
+export default App
